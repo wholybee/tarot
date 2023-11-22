3a2abe72d4dab721aa4f2517309c5570eb9e3f0a
Warren Holybee <warren@holybee.net>
2023-11-21 18:59:56 -0800

    very minor layout tweaks

--- app/src/main/java/net/holybee/tarot/holybeeAPI/HolybeeURL.kt
+++ app/src/main/java/net/holybee/tarot/holybeeAPI/HolybeeURL.kt
@@ -5,7 +5,7 @@ object HolybeeURL {
     const val login = "https://app.holybee.net/api/login/"
     const val create = "https://app.holybee.net/admin/addUser/"
     const val getCoins = "https://app.holybee.net/api/getCoins/"
-    const val openAI = "http://10.21.2.44:5000/v1/"      // "https://app.holybee.net/v1/"  "http://192.168.33.76:5000/v1/"
+    const val openAI = "https://app.holybee.net/v1/" // "http://10.21.2.44:5000/v1/"      // "https://app.holybee.net/v1/"  "http://192.168.33.76:5000/v1/"
     const val forgotPasswordURL = "https://app.holybee.net/forgotPassword"
 }
 /*
