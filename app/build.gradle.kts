b0e4554cac4760f1089f7c878e0f9b7ac048cfaa
Warren Holybee <warren@holybee.net>
2023-11-21 23:49:49 -0800

    bumped version code to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.holybee.tarot"
         minSdk = 27
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
